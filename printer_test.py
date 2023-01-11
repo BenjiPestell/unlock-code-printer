8d82abb0b50f432933adb47a64d0e509d2780344
BenjiPestell <89073517+BenjiPestell@users.noreply.github.com>
2023-01-11 16:32:12 +0000

    Added comments

--- printer_test.py
+++ printer_test.py
@@ -3,17 +3,22 @@ from escpos.printer import Usb
 
 def print_unlock_receipt(unlock_code):
 	try:
+		#Establish connection with printer
 		p = Usb(0x0416, 0x5011)
-		#p.text("\n")
-		p.image("Yeti head.png")
+		#Print top logo
+		p.image("Yeti logo.png")
+		#Print "PrecisionPro + Unlock code"
 		p.set("CENTER","A","normal",2,2,True,False)
 		p.text("\n\n\nPrecisionPro +")
 		p.set("CENTER","A","B",2,2,True,False)
 		p.text("\nUnlock Code:\n\n")
+		#Print Unlock code
 		p.set("CENTER","A","B",2,2,True,True)
 		p.text(" " + str(unlock_code) +" " + "\n\n\n")
+		#Print do not descard warning
 		p.image("do not discard.png")
 		p.text("\n\n\n\n")
+		#Close connection with printer
 		p.close()
 		print("Printing complete")
 	except:
